b3358043d6a76445342bb1e84eef5b765923bc86
RahulGonal <76273611+RahulGonal@users.noreply.github.com>
2021-01-29 10:25:55 +0530

    Update AsciiConvrt.py

--- AsciiConvrt.py
+++ AsciiConvrt.py
@@ -1,3 +1,3 @@
 import ascii_magic
-Ascci = ascii_magic.from_image_file(r"C:\Users\Dell pc\Desktop\RAHUL\CodingAndProjects\NewLogo.png",columns=100,char="#")
+Ascci = ascii_magic.from_image_file(r"Path_Of_The_Image_You_Want_To_Convert_to_Ascii",columns=100,char="#")
 ascii_magic.to_terminal(Ascci)
